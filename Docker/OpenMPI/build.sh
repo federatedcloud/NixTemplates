9acf0fdaa7feb9e0f7a427e6d054cb4dbed0e607
Brandon Elam Barker <beb82@cornell.edu>
2018-06-05 14:14:20 +0000

    added versioning of nix expressions

--- Docker/OpenMPI/build.sh
+++ Docker/OpenMPI/build.sh
@@ -11,3 +11,9 @@ echo "NIX_OMPI_IMAGE is $NIX_OMPI_IMAGE"
 docker build \
        --build-arg BASEDIR="$BASEDIR" \
        -t "$NIX_OMPI_IMAGE" -f "$BASEDIR/Dockerfile" .
+
+TEST_IMG="${REPO}_${TAG}_TEST"
+docker create --name "$TEST_IMG" "$NIX_OMPI_IMAGE"
+docker cp "$TEST_IMG:/tmp/.nix_versions" "$BASEDIR"
+docker cp "$TEST_IMG:/tmp/env_backup.drv" "$BASEDIR"
+docker rm -f "$TEST_IMG"
