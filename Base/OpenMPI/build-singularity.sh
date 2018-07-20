5a14a0d9988b1d0dfe2a8a5c2ad2f12595d5d6c8
Brandon Barker <beb82@cornell.edu>
2018-07-20 10:43:29 -0400

    reverting singularity build to happen from repo root

--- Base/OpenMPI/build-singularity.sh
+++ Base/OpenMPI/build-singularity.sh
@@ -3,9 +3,6 @@
 GITROOT=$(git rev-parse --show-toplevel)
 BASEDIR=$(dirname "${BASH_SOURCE[0]}")
 export BASEDIR
-ROOTREL=$(realpath --relative-to="$BASEDIR" "$GITROOT")
-export ROOTREL
-
 
 #
 # **** Pick a Distro to build below by uncommenting a section ****
@@ -25,5 +22,5 @@ echo "NIX_OMPI_IMAGE is $NIX_OMPI_IMAGE"
 
 # echo "SINGULARITY_DOCKER_USERNAME is set to ${SINGULARITY_DOCKER_USERNAME}"
 # echo "SINGULARITY_DOCKER_PASSWORD is set to ${SINGULARITY_DOCKER_PASSWORD}"
-cat "SingTemplate" | envsubst '${ROOTREL} ${BASEOS} ${ENVSDIR}' > "Singularity.${NIX_OMPI_IMAGE}"
-sudo singularity --debug build "${NIX_OMPI_IMAGE}.img" "Singularity.${NIX_OMPI_IMAGE}"
+cat "${BASEDIR}/SingTemplate" | envsubst '${BASEDIR} ${BASEOS} ${ENVSDIR}' > "${BASEDIR}/Singularity.${NIX_OMPI_IMAGE}"
+sudo singularity --debug build "${BASEDIR}/${NIX_OMPI_IMAGE}.img" "${BASEDIR}/Singularity.${NIX_OMPI_IMAGE}"
